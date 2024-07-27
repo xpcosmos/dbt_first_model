c84da850d95075789139f0b5e83972c9290f567f
xpcosmos <mikeias.d.s.o@gmail.com>
2024-07-27 15:27:47 -0300

    Dividing schemas in the database within the etl process

--- etl/etl.py
+++ etl/etl.py
@@ -23,26 +23,32 @@ conn_string = "postgresql+psycopg2://{}:{}@{}/{}".format(
 
 engine = create_engine(conn_string, echo=False)
 
+schemas = ['jaffle_shop', 'stripe']
+schema_files = {}
 
-csv_files = os.listdir('data')
 success = 0
+total_files = 0
 
-for csv_file in csv_files:
-    table_name = csv_file.removesuffix('.csv')
-    table_name = table_name.removeprefix('raw_')
+for k in schemas:
+    schema_files[k] = os.listdir(f'data/{k}')
 
-    df = pd.read_csv(f'data/{csv_file}')
-    attempts = 0
-    while attempts < 3:
-        try:
-            df.to_sql(name=table_name,schema='jaffle_shop', con=engine, if_exists='replace', index=False)
-            success+=1
-            break
-        except OperationalError as erro:
-            attempts += 1
-            time.sleep(SLEEP_TIME)
-            print (f"OperationalError: {erro}")
+for k, csv_files in schema_files.items():
+    for csv_file in csv_files:
+        table_name = csv_file.removesuffix('.csv')
+        table_name = table_name.removeprefix('raw_')
+
+        df = pd.read_csv(f'data/{k}/{csv_file}')
+        attempts = 0
+        while attempts < 3:
+            try:
+                df.to_sql(name=table_name,schema=k, con=engine, if_exists='replace', index=False)
+                success+=1
+                break
+            except OperationalError as erro:
+                attempts += 1
+                time.sleep(SLEEP_TIME)
+                print (f"OperationalError: {erro}")
 
 
 
-print(f'success in {success}/{len(csv_files)}')
+print(f'success in {success}/')
